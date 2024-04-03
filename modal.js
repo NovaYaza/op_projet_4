f72f2c6fa13308a134052dfde3745225b9adb3a9
NovaYaza <sefa151000@gmail.com>
2024-04-03 10:06:16 +0200

    ajout message d'erreur pour le prenom, le nom, l'email et les boutons de choix pour les tournois / effacement des messages d'erreurs lorsque l'utilisateur rentre les bonnes infos et effacement des messages d'erreurs lors de la fermeture de la modale

--- modal.js
+++ modal.js
@@ -24,7 +24,19 @@ function launchModal() {
 
 // Ferme la modal
 function closeModal() {
+  document.querySelector('form').reset(); //reset des éléments rentrés par l'utilisateur lors de la fermeture de la modal
+  //reset des messages d'erreur lors de la fermeture de la modal
+  const dataErrors = document.querySelectorAll('.formData');
+  dataErrors.forEach((dataErrors) => {
+    dataErrors.setAttribute('data-error-visible', 'false');
+    dataErrors.setAttribute('data-error', '');
+  });
   modalbg.style.display = 'none';
+}
+
+//Nettoyage des messages d'erreurs formulaire
+function clearErrorMesssage() {
+
 }
 
 function validate(event) {
@@ -33,49 +45,100 @@ function validate(event) {
 
   let error = "";
   //verification
+  const eltfirstName = document.getElementById('first');
+  const eltlastName = document.getElementById('last');
+  const eltEmail = document.getElementById('email');
+  const testEspacefirstName = eltfirstName.value.trim();
+  const testEspacelastName = eltlastName.value.trim();
+  const testEspaceEmail = eltEmail.value.trim();
   //prenom
-  if (!new RegExp('^[a-zA-ZÀ-ÿ- ]{2,}$').test(document.getElementById('first').value)) {
-    error.setAttribute('data-error-visible', 'true');/// Pour les messages d'erreur il faut utiliser setattribute et le data-error dans le css
+  if (!new RegExp('^[a-zA-ZÀ-ÿ- ]{2,}$').test(testEspacefirstName)) {
+    eltfirstName.parentElement.setAttribute('data-error-visible', 'true');
+    eltfirstName.parentElement.setAttribute('data-error', 'Veuillez entrer 2 caractères ou plus pour le champ du prénom.')
     /* error += "Veuillez entrer 2 caractères ou plus pour le champ du prénom."; */
+  } else {
+    eltfirstName.parentElement.setAttribute('data-error-visible', 'false');
+    eltfirstName.parentElement.setAttribute('data-error', '')
   }
   //nom
-  if (!new RegExp('^[a-zA-ZÀ-ÿ\u00C0-\u00FF\u0100-\u017F\u0180-\u024F\u1E00-\u1EFF-]{2,}$').test(document.getElementById('last').value)) {
-    error += "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
+  if (!new RegExp('^[a-zA-ZÀ-ÿ- ]{2,}$').test(testEspacelastName)) {
+    eltlastName.parentElement.setAttribute('data-error-visible', 'true');
+    eltlastName.parentElement.setAttribute('data-error', 'Veuillez entrer 2 caractères ou plus pour le champ du nom.')
+    /* error += "Veuillez entrer 2 caractères ou plus pour le champ du nom."; */
+  } else {
+    eltlastName.parentElement.setAttribute('data-error-visible', 'false');
+    eltlastName.parentElement.setAttribute('data-error', '')
   }
   //email
-  if (!new RegExp('[a-z0-9._-]+@[a-z0-9._-]+\.[a-z0-9._-]+').test(document.getElementById('email').value)) {
-    error += "Veuillez entrer un email valide";
+  if (!new RegExp('[a-z0-9._-]+@[a-z0-9._-]+\.[a-z0-9._-]+').test(testEspaceEmail)) {
+    eltEmail.parentElement.setAttribute('data-error-visible', 'true');
+    eltEmail.parentElement.setAttribute('data-error', 'Veuillez entrer un email valide.')
+    /* error += "Veuillez entrer un email valide"; */
+  } else {
+    eltEmail.parentElement.setAttribute('data-error-visible', 'false');
+    eltEmail.parentElement.setAttribute('data-error', '')
   }
   //date de naissance
   if (!new RegExp('^\\d{2}/\\d{2}/\\d{4}$').test(document.getElementById('birthdate').value) /* && dateNaissanceObj <= dateActuelle */) {
-    error += "Vous devez entrer votre date de naissance.";
+    /* error += "Vous devez entrer votre date de naissance."; */
   }
+  //Choix boutons radio
+  const radioLocations = document.querySelectorAll('input[type=radio][name="location"]');
+  let isChecked = false;
 
-  if (error === "") {
-    //affichage
-    //prenom
-    
-    //nom
+  radioLocations.forEach(function(radioButton) {
+    if (radioButton.checked) {
+      isChecked = true;
+    }
+  });
 
-    //email
-
-    //date naissance
-
-    //Conditions et newsletter
-    let checkbox_values = []
+  if (!isChecked) {
+    radioLocations.forEach(function(radioButton) {
+      radioButton.parentElement.setAttribute('data-error-visible', 'true');
+      radioButton.parentElement.setAttribute('data-error', 'Veuillez sélectionner un tournoi.');
+    });
+  } else {
+    radioLocations.forEach(function(radioButton) {
+      radioButton.parentElement.setAttribute('data-error-visible', 'false');
+      radioButton.parentElement.setAttribute('data-error', '');
+    });
+}
+  //Conditions et newsletter
+  /* let checkbox_values = []
     for (let input of document.querySelector('.checkbox2-label')) {
       if (input.type == "checkbox" && input.checked) { checkbox_values.push(input.value) }
-    }
-    /*Affichage erreur*/
-  console.log()
-}
-else {
-  window.alert(error);
-}
+    } */
 }
 
 // Ferme la modal au clique en appelant ma fonction closeModal
 document.querySelector('.close').addEventListener('click', closeModal);
+
+
+/* if (error === "") {
+  //affichage
+  //prenom
+  
+  //nom
+
+  //email
+
+  //date naissance
+
+  //Conditions et newsletter
+  let checkbox_values = []
+  for (let input of document.querySelector('.checkbox2-label')) {
+    if (input.type == "checkbox" && input.checked) { checkbox_values.push(input.value) }
+  }
+  Affichage erreur
+console.log()
+}
+else {
+window.alert(error);
+} */
+
+
+
+
 
 // Vérification du contenu entré par l'utilisateur                                                                                                                                                                                      
 	/* document.querySelector('.btn-submit').addEventListener( (event) => {
@@ -90,3 +153,6 @@ document.querySelector('.close').addEventListener('click', closeModal);
 
 let closeModal = document.getElementsByClassName(".close");
 modalBtn.forEach((closeModal) => closeModal.addEventListener("click", closeModal)); */
+
+
+/* ^[a-zA-ZÀ-ÿ\u00C0-\u00FF\u0100-\u017F\u0180-\u024F\u1E00-\u1EFF-]{2,}$ */
